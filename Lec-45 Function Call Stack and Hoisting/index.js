6215743e421e2d4ec3cf0f33d869024dcefdcfe6
Abdul Rahman Ali <abdulahmanali4778@gmail.com>
2025-10-24 21:39:51 +0530

    🧠 Lec-45: Function Call Stack & Hoisting in JavaScript
    • Learned function declaration, expression, and arrow syntax
    • Explored callback & higher-order functions
    • Understood execution context and call stack management
    • Studied hoisting and how JS preloads declarations
    
    ✨ Key Insight: JS reads first, runs later — mastering stack flow simplifies debugging.

---
+++ Lec-45 Function Call Stack and Hoisting/index.js
@@ -0,0 +1,57 @@
+// function sayMyName(fileName){
+//     console.log(fileName);
+// }
+// sayMyName("Abdul ")
+ 
+// function stack
+
+// function greetme(greet, fullName){
+//     console.log("Hellow",fullName);
+//     greet();
+// }
+// let greet = function(){
+//     console.log("Good Morning");
+// }
+// greetme(greet, "Abdul Rahman");
+
+// function solve(number){
+//     return function(number){
+//         return number*number;
+//     }
+// }
+// let ans =solve(5);
+// let finalans = ans(10);
+// console.log(finalans);
+
+// const arr = [
+//     function(a,b){
+//         return a+b;
+//     },
+//     function (a,b){
+//         return a-b;
+//     },
+//     function (a,b){
+//         return a*b;
+//     }
+// ]
+// // let first = arr[0];
+// // let first = arr[1];
+// // let first = arr[2];
+// let ans = first(5,10);
+// console.log(ans);
+
+// let obj = {
+//     age:25,
+//     wr: 38,
+//     ht:148,
+//     greet:()=>{
+//         console.log("Hellow Abdul");
+//     }
+// }
+// console.log(obj.age)
+// obj.greet();
+
+// console.log(greet);
+// var greet = function(){
+//     console.log("Hellow");
+// }
