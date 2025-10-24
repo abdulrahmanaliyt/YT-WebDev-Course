c0331f68d4910d691f4e754da2d06837c289989d
Abdul Rahman Ali <abdulahmanali4778@gmail.com>
2025-10-24 21:41:06 +0530

    📦 Lec-46: Variable Scope & Temporal Dead Zone (TDZ)
    • Understood global, block, and function scopes
    • Differentiated var, let, and const behavior
    • Explored TDZ and how it prevents premature access
    • Visualized scope chain and execution hierarchy
    
    ✨ Key Insight: Scope defines accessibility — TDZ enforces disciplined declarations.

---
+++ Lec-46 Variable Scope and Temporal Dead Zone in JS/index.js
@@ -0,0 +1,42 @@
+// // global scope
+// // var age = 15;
+// // let age = 15;
+// const age = 15;
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+// if(true){
+//     console.log(age);
+// }
+// for(let i=0; i<2; i++){
+//     console.log(age);
+// }
+
+// function sayHello(){
+//     console.log(age+1);
+// }
+// sayHello();
+
+// // function scope
+// function sayHello(){
+//     var name = 'Earth';
+//     console.log("Hello Duniya Valo of ",name);
+// }
+// sayHello();
+
+// // block scope
+// {
+//     // let age = 100;
+//     const age = 38;
+// }
+// console.log(age);
+
+// // Temporal Dead Zone
+// //ex:
+// console.log(markd);
+// console.log("Abdul");
+// console.log("Rahman");
+// const marks = 100; 
+// console.log(marks);
