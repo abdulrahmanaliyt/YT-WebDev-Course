b21a7586444b25c02873b3d5c81d0a71bf8929eb
Abdul Rahman Ali <abdulahmanali4778@gmail.com>
2025-10-26 19:25:48 +0530

    🧮 Lec-48: In-Built Objects in JavaScript
    • Explored Math and Date objects
    • Learned key methods: round, floor, random, sqrt, pow
    • Practiced date creation and manipulation using new Date()
    • Understood built-in constants like PI and E
    
    ✨ Key Insight: Math & Date bring logic and time handling power to JS natively.

---
+++ Lec-48 In-Built Objects in JS/index.js
@@ -0,0 +1,162 @@
+console.log("Hello Welcone to today lecture");
+
+// // Math Object
+// console.log(Math.PI);
+// console.log("Max ->",Math.max(1,2,3,4,5,53,63,26,6,4,67,7,7,74));
+// console.log("Min ->", Math.min(1, 2, 3, 4, 5, 53, 63, 26, 6, 4, 67, 7, 7, 74));
+// console.log("Round of 2.4 -> ",Math.round(2.4));
+// console.log("Floor of 3.5 ->",Math.floor(3.5));
+// console.log("Ceil of 1.3 -> ",Math.ceil(1.3));
+// console.log("abd Absolute convert all - value to + ex -34 ->",Math.abs(-34));
+// console.log("Random no b/w (0 & 1) ->",Math.random());
+// console.log("Square root of 5 ->",Math.sqrt(5));
+// console.log("Power of 5 to 2 ->",Math.pow(5,2))
+
+// Date Object
+let curr = new Date();
+let date = new Date('July 04 2000 7:15');
+let newDate = new Date(1998, 5, 7)
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.setFullYear(2003))
+console.log(newDate)
+
+// // Static properties
+// Math.E
+// Euler's number and the base of natural logarithms; approximately 2.718.
+
+// Math.LN10
+// Natural logarithm of 10; approximately 2.303.
+
+// Math.LN2
+// Natural logarithm of 2; approximately 0.693.
+
+// Math.LOG10E
+// Base-10 logarithm of E; approximately 0.434.
+
+// Math.LOG2E
+// Base-2 logarithm of E; approximately 1.443.
+
+// Math.PI
+// Ratio of a circle's circumference to its diameter; approximately 3.14159.
+
+// Math.SQRT1_2
+// Square root of ½; approximately 0.707.
+
+// Math.SQRT2
+// Square root of 2; approximately 1.414.
+
+// Math[Symbol.toStringTag]
+// The initial value of the [Symbol.toStringTag] property is the string "Math". This property is used in Object.prototype.toString().
+
+// Static methods
+// Math.abs()
+// Returns the absolute value of the input.
+
+// Math.acos()
+// Returns the arccosine of the input.
+
+// Math.acosh()
+// Returns the hyperbolic arccosine of the input.
+
+// Math.asin()
+// Returns the arcsine of the input.
+
+// Math.asinh()
+// Returns the hyperbolic arcsine of a number.
+
+// Math.atan()
+// Returns the arctangent of the input.
+
+// Math.atan2()
+// Returns the arctangent of the quotient of its arguments.
+
+// Math.atanh()
+// Returns the hyperbolic arctangent of the input.
+
+// Math.cbrt()
+// Returns the cube root of the input.
+
+// Math.ceil()
+// Returns the smallest integer greater than or equal to the input.
+
+// Math.clz32()
+// Returns the number of leading zero bits of the 32-bit integer input.
+
+// Math.cos()
+// Returns the cosine of the input.
+
+// Math.cosh()
+// Returns the hyperbolic cosine of the input.
+
+// Math.exp()
+// Returns ex, where x is the argument, and e is Euler's number (2.718…, the base of the natural logarithm).
+
+// Math.expm1()
+// Returns subtracting 1 from exp(x).
+
+// Math.floor()
+// Returns the largest integer less than or equal to the input.
+
+// Math.f16round()
+// Returns the nearest half precision float representation of the input.
+
+// Math.fround()
+// Returns the nearest single precision float representation of the input.
+
+// Math.hypot()
+// Returns the square root of the sum of squares of its arguments.
+
+// Math.imul()
+// Returns the result of the 32-bit integer multiplication of the inputs.
+
+// Math.log()
+// Returns the natural logarithm (㏒e; also, ㏑) of the input.
+
+// Math.log10()
+// Returns the base-10 logarithm of the input.
+
+// Math.log1p()
+// Returns the natural logarithm (㏒e; also ㏑) of 1 + x for the number x.
+
+// Math.log2()
+// Returns the base-2 logarithm of the input.
+
+// Math.max()
+// Returns the largest of zero or more numbers.
+
+// Math.min()
+// Returns the smallest of zero or more numbers.
+
+// Math.pow()
+// Returns base x to the exponent power y (that is, xy).
+
+// Math.random()
+// Returns a pseudo-random number between 0 and 1.
+
+// Math.round()
+// Returns the value of the input rounded to the nearest integer.
+
+// Math.sign()
+// Returns the sign of the input, indicating whether it is positive, negative, or zero.
+
+// Math.sin()
+// Returns the sine of the input.
+
+// Math.sinh()
+// Returns the hyperbolic sine of the input.
+
+// Math.sqrt()
+// Returns the positive square root of the input.
+
+// Math.sumPrecise()
+// Returns the sum of a passed iterable of numbers, avoiding floating point precision loss in intermediate results.
+
+// Math.tan()
+// Returns the tangent of the input.
+
+// Math.tanh()
+// Returns the hyperbolic tangent of the input.
+
+// Math.trunc()
+// Returns the integer portion of the input, removing any fractional digits.
