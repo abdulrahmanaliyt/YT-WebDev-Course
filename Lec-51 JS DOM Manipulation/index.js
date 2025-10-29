185d028d799bcb867b43c7458e1caa4a4cddbd13
Abdul Rahman Ali <abdulahmanali4778@gmail.com>
2025-10-29 23:01:30 +0530

    🌐 Lec-51: JavaScript DOM Manipulation
    • Accessed HTML elements using getElementById, querySelector
    • Modified content via innerHTML and textContent
    • Added elements dynamically using createElement & appendChild
    • Practiced element removal and insertion (insertAdjacentElement)
    • Understood how JS interacts with the browser DOM
    
    ✨ Key Insight: DOM manipulation connects JS logic with HTML structure — the essence of dynamic web pages.

---
+++ Lec-51 JS DOM Manipulation/index.js
@@ -0,0 +1,16 @@
+
+// insertAdjacentElement
+
+// let mydiv = document.querySelector('#mydiv');
+
+// let newElement = document.createElement('span');
+// newElement.textContent = 'Abdul Rahman Ali';
+
+// // mydiv.insertAdjacentElement('beforebegin',newElement);
+// // mydiv.insertAdjacentElement("afterbegin", newElement);
+// // mydiv.insertAdjacentElement("beforeend", newElement);
+// mydiv.insertAdjacentElement("afterend", newElement);
+
+// let parent = document.querySelector('#mydiv');
+// let child = document.querySelector('#fpara');
+// parent.removeChild(child);
