02109d9f306eb237d974609adba43c6fc55d4e39
Abdul Rahman Ali <abdulahmanali4778@gmail.com>
2025-10-29 23:27:58 +0530

    🎨 Lec-52: DOM Traversal & Styling in JavaScript
    • Practiced element selection using getElementById and querySelector
    • Modified styles dynamically with .style and .cssText
    • Used setAttribute to update element properties
    • Controlled class lists using add, remove, toggle, and contains
    • Understood how JS can dynamically alter structure and appearance
    
    ✨ Key Insight: JavaScript empowers live control over the DOM — structure, behavior, and style united.

---
+++ Lec-52 DOM Tree Traversal & Styling in JS/index.js
@@ -0,0 +1,40 @@
+// // // // let paraElement = document.getElementById('spara');
+// // // // console.log(paraElement);
+// // // // paraElement.style.backgroundColor = 'blue';
+// // // // paraElement.style.padding = "2rem";
+// // // // paraElement.innerText = "Style Changed!";
+
+// // // let secondElement = document.getElementById('sdiv');
+// // // // secondElement.style.cssText;
+// // // secondElement.style.cssText = "background-color:black; color:white; padding.5rem";
+
+// // let firstElement = document.querySelector('#fdiv');
+// // // firstElement;
+
+// // firstElement.setAttribute('class','divClass');
+// // // firstElement
+// // firstElement.setAttribute('class','secondDivClass');
+// // // firstElement
+// // // firstElement.setAttribute("style", "padding:.1rem");
+// // // firstElement
+// // fpara.setAttribute('style', 'margin:1rem');
+
+// // let fpara = $0;
+// // fpara
+// // fpara.className
+// // fpara.className = "Gohan Bulbul";
+// // fpara.className;
+
+// let fpara = document.querySelector('#fpara');
+// fpara.classList;
+// fpara.classList.add('thirdClass');
+// fpara.classList.remove("fclass");
+// fpara.classList.toggle('sclass');
+// fpara.classList;
+// fpara.classList.toggle('sclass');
+// fpara.classList;
+// fpara.classList.toggle("sclass");
+// fpara.classList.contains("fclass");
+// fpara.classList.contains("tclass");
+
+// // let fpara = document.querySelector('#fpara');
